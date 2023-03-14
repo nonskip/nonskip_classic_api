72d369d848dab6d1dde89decf29c48548e19b698
himychae <nyong10@naver.com>
2023-03-14 20:26:24 +0900

    suggest 0314

--- readpilot/func_suggest.py
+++ readpilot/func_suggest.py
@@ -2,4 +2,15 @@ from . import Dialogue
 
 
 def suggest(d: Dialogue) -> list[str]:
-    pass
+    prompt = "Considering this reader's previous Q&A history and the common difficulties in reading foreign language texts, please suggest five questions that the reader is most likely to curious about especially in terms of expression usages. \
+        Please encourage extensive learning not to be just limited to this text itself. \
+        Exclude any unnecessary comment except for your suggestions while answering."
+        
+    response = openai.ChatCompletion.create(
+        model = d.model,
+        messages = [{"role": "assistant", "content": prompt}]
+    )
+    result = response["choices"][0].message.content.split('\n')
+    
+    
+    return result
