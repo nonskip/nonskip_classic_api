acb227c92996532f58a4cd673b15bafa1dc2b5fe
aeoebe <aeoebe3057@naver.com>
2023-03-14 20:56:19 +0900

    suggest

--- readpilot/func_suggest.py
+++ readpilot/func_suggest.py
@@ -1,5 +1,22 @@
 from . import Dialogue
+import openai
+
+
+def ask_chatgpt(d: Dialogue):
+    prompt = """
+    Suggest 5 probable questions about the expression or the word based on the context. 
+    Only return the questions without any other comments.
+    """
+    model = d.model
+    chat_completion = openai.ChatCompletion.create(
+        model=model, messages=[{"role": "user", "context": prompt}]
+    )
+    response = chat_completion.choices[0].message.content
+    suggest_questions = response.split('\n')
+    return suggest_questions
 
 
 def suggest(d: Dialogue) -> list[str]:
-    pass
+    result = ask_chatgpt(d)
+    return result
+
