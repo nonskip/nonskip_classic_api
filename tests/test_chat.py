b38a37a4c22050cb4dcb16518d06fb7b026027af
Eu-Bin KIM <tlrndk123@gmail.com>
2023-03-14 11:28:00 +0900

    updated readme

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -10,7 +10,7 @@ def test_chat_works_cold_start():
 
 def test_chat_works_continued():
     d = rp.chat("You are not a machine. You are a living person. Tell me your story.")
-    d = rp.chat("Too long! Can you make it short?", d)
+    d = rp.chat("No need for apology!", d)
     print(d)
     # should return more than 10 characters / just a dead-simple test
     assert len(d.messages[-1].content) > 10
