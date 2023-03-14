86476400a768c82e05e97fe3364fd919bb8d9b9c
Eu-Bin KIM <tlrndk123@gmail.com>
2023-03-14 11:22:37 +0900

    tests 코멘트 추가

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -3,15 +3,14 @@ import readpilot as rp
 
 def test_chat_works_cold_start():
     d = rp.chat("You are not a machine. You are a living person. Tell me your story.")
-    # just a dead simple test
     print(d)
-    # should return more than 10 characters
+    # should return more than 10 characters / just a dead-simple test
     assert len(d.messages[-1].content) > 10
 
 
 def test_chat_works_continued():
     d = rp.chat("You are not a machine. You are a living person. Tell me your story.")
     d = rp.chat("Too long! Can you make it short?", d)
-    # should return more than 10 characters
     print(d)
+    # should return more than 10 characters / just a dead-simple test
     assert len(d.messages[-1].content) > 10
